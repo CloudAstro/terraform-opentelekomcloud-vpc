6e646238cce4dd76501b170eda361e87918f3991
dibranica <erjon.dibrani@cloudastro.de>
2026-06-17 14:47:24 +0200

    feat: bump tf and provider version

--- examples/default/terrafrom.tf
+++ examples/default/terrafrom.tf
@@ -1,8 +1,9 @@
 terraform {
+  required_version = ">= 1.12"
   required_providers {
     opentelekomcloud = {
       source  = "opentelekomcloud/opentelekomcloud"
-      version = ">= 1.36.35"
+      version = ">= 1.36.68"
     }
   }
 }
